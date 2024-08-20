3b85fb1b225aa80aacc0ea61d153f1e43ebcc2ba
Tijana Kvaic <68079009+tijanak@users.noreply.github.com>
2024-08-20 23:39:43 +0200

    stilizovanje

--- frontend/src/app/components/sale-certificates/sale-certificate/sale-certificate.component.ts
+++ frontend/src/app/components/sale-certificates/sale-certificate/sale-certificate.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ISale_Certificate } from '@org/models';
 import { ImageGalleryComponent } from '../../image-gallery/image-gallery.component';
+import { UserComponent } from '../../user/user.component';
 
 @Component({
   selector: 'app-sale-certificate',
   standalone: true,
-  imports: [CommonModule, MatCardModule, ImageGalleryComponent],
+  imports: [CommonModule, MatCardModule, ImageGalleryComponent, UserComponent],
   templateUrl: './sale-certificate.component.html',
   styleUrl: './sale-certificate.component.css',
 })
